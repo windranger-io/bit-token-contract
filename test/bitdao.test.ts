8ff9ca8aa36281c817164c04414b3ad1a7c43ead
CJ Hare <CjHare@users.noreply.github.com>
2021-09-08 16:38:00 +1000

    feat: Anonymous arrow functions (#7)

--- test/bitdao.test.ts
+++ test/bitdao.test.ts
@@ -1,13 +1,13 @@
-// Start - Support Mocha level debugging
+// Start - Support direct Mocha run & debug
 import "hardhat"
 import "@nomiclabs/hardhat-ethers";
-// End - Support Mocha level debugging
+// End - Support direct Mocha run & debug
 
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("BitDAO", async function () {
-    it("Name should be BitDAO", async function (){
+describe("BitDAO", async  ()=> {
+    it("Name should be BitDAO", async ()=>{
         const adminAccount = await signer()
         const BitDAO = await ethers.getContractFactory("BitDAO");
         const dao = await BitDAO.deploy(adminAccount);
@@ -16,7 +16,7 @@ describe("BitDAO", async function () {
         expect(await dao.name()).to.equal('BitDAO')
     })
 
-    it("Zero votes for the admin", async function () {
+    it("Zero votes for the admin", async () =>{
         const adminAccount = await signer()
         const BitDAO = await ethers.getContractFactory("BitDAO");
         const dao = await BitDAO.deploy(adminAccount);
@@ -27,7 +27,7 @@ describe("BitDAO", async function () {
     });
 });
 
-async function signer(){
+async function signer():Promise<string>{
     const signers = await ethers.getSigners()
     expect(   signers.length).is.greaterThan(1)
     return signers[0].address
