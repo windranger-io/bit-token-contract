00b1b6fd93b0113edb8324ba8a147397ff0bc31e
CJ Hare <CjHare@users.noreply.github.com>
2021-09-08 18:14:56 +1000

    feat: Linter and Formatter (#8)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,33 +1,29 @@
-import { task } from "hardhat/config";
+import {task} from 'hardhat/config'
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(await account.address);
+    console.log(await account.address)
   }
-});
+})
 
 // task action function receives the Hardhat Runtime Environment as second argument
-task(
-    "blockNumber",
-    "Prints the current block number",
-    async (_, { ethers }) => {
-      await ethers.provider.getBlockNumber().then((blockNumber) => {
-        console.log("Current block number: " + blockNumber);
-      });
-    }
-);
+task('blockNumber', 'Prints the current block number', async (_, {ethers}) => {
+  await ethers.provider.getBlockNumber().then((blockNumber) => {
+    console.log('Current block number: ' + blockNumber)
+  })
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 // Last Solidity for 0.7.x stream is 0.7.6
 export default {
-  solidity: "0.7.6",
-};
+  solidity: '0.7.6'
+}
--- test/bitdao.test.ts
+++ test/bitdao.test.ts
@@ -1,34 +1,33 @@
 // Start - Support direct Mocha run & debug
-import "hardhat"
-import "@nomiclabs/hardhat-ethers";
+import 'hardhat'
+import '@nomiclabs/hardhat-ethers'
 // End - Support direct Mocha run & debug
 
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import {expect} from 'chai'
+import {ethers} from 'hardhat'
 
-describe("BitDAO", async  ()=> {
-    it("Name should be BitDAO", async ()=>{
-        const adminAccount = await signer()
-        const BitDAO = await ethers.getContractFactory("BitDAO");
-        const dao = await BitDAO.deploy(adminAccount);
-        await dao.deployed();
+describe('BitDAO', async () => {
+  it('Name should be BitDAO', async () => {
+    const adminAccount = await signer()
+    const BitDAO = await ethers.getContractFactory('BitDAO')
+    const dao = await BitDAO.deploy(adminAccount)
+    await dao.deployed()
 
-        expect(await dao.name()).to.equal('BitDAO')
-    })
+    expect(await dao.name()).to.equal('BitDAO')
+  })
 
-    it("Zero votes for the admin", async () =>{
-        const adminAccount = await signer()
-        const BitDAO = await ethers.getContractFactory("BitDAO");
-        const dao = await BitDAO.deploy(adminAccount);
-        await dao.deployed();
+  it('Zero votes for the admin', async () => {
+    const adminAccount = await signer()
+    const BitDAO = await ethers.getContractFactory('BitDAO')
+    const dao = await BitDAO.deploy(adminAccount)
+    await dao.deployed()
 
+    expect(await dao.getCurrentVotes(adminAccount)).to.equal(0)
+  })
+})
 
-        expect(await dao.getCurrentVotes(adminAccount)).to.equal(0)
-    });
-});
-
-async function signer():Promise<string>{
-    const signers = await ethers.getSigners()
-    expect(   signers.length).is.greaterThan(1)
-    return signers[0].address
+async function signer(): Promise<string> {
+  const signers = await ethers.getSigners()
+  expect(signers.length).is.greaterThan(1)
+  return signers[0].address
 }
